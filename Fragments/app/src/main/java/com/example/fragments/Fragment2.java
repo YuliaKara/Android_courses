3ead459952540f64b50a94ba91288751e56be330
Yulia <Yullev@rambler.ru>
2021-12-05 16:47:57 +0300

    Fragments

--- Fragments/app/src/main/java/com/example/fragments/Fragment2.java
+++ Fragments/app/src/main/java/com/example/fragments/Fragment2.java
@@ -1,2 +1,17 @@
-package com.example.fragments;public class Fragment2 {
+package com.example.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class Fragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmen2,container,false);
+    }
 }
